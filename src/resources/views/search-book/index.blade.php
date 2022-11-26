e708b84aca111f64dae27dee561f1c127999181a
masa0307 <masayuki.030737@gmail.com>
2022-11-27 05:40:59 +0900

    [fix]本の内部検索にて、該当する本がなかったときに発生する不具合を修正

--- src/resources/views/search-book/index.blade.php
+++ src/resources/views/search-book/index.blade.php
@@ -22,27 +22,27 @@
             <div class="hidden md:block mr-4 mt-8">
                 <ul>
                     <li>
-                        @if(session("search_books"))
+                        @if(session("search_books") && $is_search_result)
                             <h3 class="pl-4 md:text-subtitle">検索結果</h3>
                             <p class="pl-6 pt-2 text-normal">読書中</p>
                             <ul class="pl-10 text-normal">
 
-                            @foreach (session("search_books") as $book_reading)
-                                <li class="mt-2">
-                                    <a href="{{route('search-book.show', [$book_reading->id,  str_replace('?', '', mb_strstr(url()->full(), '?'))])}}" class="marker block"><iconify-icon inline icon="clarity:book-line" width="16" height="16" class="mr-2"></iconify-icon>{{$book_reading->title}}</a>
-                                    <ul class="pl-6 hidden dropdown">
-                                        <li><a href="{{route('book-memo.show', [$book_reading->id, str_replace('?', '', mb_strstr(url()->full(), '?'))])}}" class="marker block">読書メモ</a></li>
-                                        <li><a href="{{route('action-list.show', [$book_reading->id, str_replace('?', '', mb_strstr(url()->full(), '?'))])}}" class="marker block">アクションリスト</a></li>
-                                        <li><a href="{{route('feedback-list.show', [$book_reading->id, str_replace('?', '', mb_strstr(url()->full(), '?'))])}}" class="marker block">振り返り</a></li>
-                                    </ul>
-                                </li>
-                            @endforeach
+                                @foreach (session("search_books") as $book_reading)
+                                    <li class="mt-2">
+                                        <a href="{{route('search-book.show', [$book_reading->id,  str_replace('?', '', mb_strstr(url()->full(), '?'))])}}" class="marker block"><iconify-icon inline icon="clarity:book-line" width="16" height="16" class="mr-2"></iconify-icon>{{$book_reading->title}}</a>
+                                        <ul class="pl-6 hidden dropdown">
+                                            <li><a href="{{route('book-memo.show', [$book_reading->id, str_replace('?', '', mb_strstr(url()->full(), '?'))])}}" class="marker block">読書メモ</a></li>
+                                            <li><a href="{{route('action-list.show', [$book_reading->id, str_replace('?', '', mb_strstr(url()->full(), '?'))])}}" class="marker block">アクションリスト</a></li>
+                                            <li><a href="{{route('feedback-list.show', [$book_reading->id, str_replace('?', '', mb_strstr(url()->full(), '?'))])}}" class="marker block">振り返り</a></li>
+                                        </ul>
+                                    </li>
+                                @endforeach
 
-                            {{ $search_books->links('vendor.pagination.custom') }}
+                                {{ $search_books->links('vendor.pagination.custom') }}
+                            </ul>
                         @else
                             <x-sp-hidden-memo-list :books-reading="$books_reading" :books-interesting="$books_interesting" :memo-groups="$memo_groups_paginate" />
                         @endif
-                        </ul>
                     </li>
                 </ul>
             </div>
