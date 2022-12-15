8a0eca1ee44bd65e65106eefbb62b89064f70128
masa0307 <masayuki.030737@gmail.com>
2022-12-15 22:25:54 +0900

    [fix]招待されているグループ名が正しく表示されない不具合を修正
    招待されているグループ名でループを回すことにより、不具合を解消した

--- src/resources/views/components/invitation.blade.php
+++ src/resources/views/components/invitation.blade.php
@@ -1,27 +1,20 @@
-<div>
-    @foreach ($invited_group_users as $count => $invited_group_user)
-        @if($count === 0)
-            <div class="fixed left-0 top-0 z-10 overflow-auto h-full w-full bg-modal-rgba">
-                <div class="modal-content-setting bg-modal-window mt-40 pb-4 w-11/12 md:w-1/4 mx-auto text-center text-xl md:text-2xl rounded-xl">
-                    <div class="bg-primary p-3 rounded-xl w-full text-center text-normal">招待通知</div>
-                    <p class="flex justify-start w-3/4 mx-auto pt-8 text-xl">招待ユーザー名：{{ $invitee_user_name }}</p>
-                    <p class="flex justify-start w-3/4 mx-auto pt-6 text-xl">招待グループ名：{{ $invtee_group_name }}</p>
-                    <div class="mt-8">
-                        <form action="{{ route('group-user.update') }}" method="post">
-                            @csrf
-                            @method('patch')
-                            <button type="submit" name="participation_status" value="参加中" class="block w-full py-4 bg-slate-300 border-b-2 border-slate-200 hover:bg-sky-500 hover:text-slate-50">参加</button>
-                        </form>
+<div class="fixed left-0 top-0 z-10 overflow-auto h-full w-full bg-modal-rgba">
+    <div class="modal-content-setting bg-modal-window mt-40 pb-4 w-11/12 md:w-1/4 mx-auto text-center text-xl md:text-2xl rounded-xl">
+        <div class="bg-primary p-3 rounded-xl w-full text-center text-normal">招待通知</div>
+        <p class="flex justify-start w-3/4 mx-auto pt-8 text-xl">招待ユーザー名：{{ $invitee_user_name }}</p>
+        <p class="flex justify-start w-3/4 mx-auto pt-6 text-xl">招待グループ名：{{ $invtee_group_name }}</p>
+        <div class="mt-8">
+            <form action="{{ route('group-user.accept') }}" method="post">
+                @csrf
+                @method('patch')
+                <button type="submit" name="participation_status" value="参加中" class="block w-full py-4 bg-slate-300 border-b-2 border-slate-200 hover:bg-sky-500 hover:text-slate-50">参加</button>
+            </form>
 
-                        <form action="{{ route('group-user.reject') }}" method="post">
-                            @csrf
-                            @method('delete')
-                            <button type="submit" name="participation_status" value="非参加" class="block w-full py-4 bg-slate-300 mb-4 hover:bg-sky-500 hover:text-slate-50">非参加</button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        @endif
-    @endforeach
+            <form action="{{ route('group-user.reject') }}" method="post">
+                @csrf
+                @method('delete')
+                <button type="submit" name="participation_status" value="非参加" class="block w-full py-4 bg-slate-300 mb-4 hover:bg-sky-500 hover:text-slate-50">非参加</button>
+            </form>
+        </div>
+    </div>
 </div>
-
