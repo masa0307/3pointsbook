a96710fdcf30c3f46e41e19ba13016971621ec3e
masa0307 <masayuki.030737@gmail.com>
2022-12-17 16:24:24 +0900

    [fix]GroupUserController内のrejectメソッドをリファクタリング

--- src/resources/views/components/invitation.blade.php
+++ src/resources/views/components/invitation.blade.php
@@ -7,7 +7,7 @@
                     <p class="flex justify-start w-3/4 mx-auto pt-8 text-xl">招待ユーザー名：{{ $invitee_user_name }}</p>
                     <p class="flex justify-start w-3/4 mx-auto pt-6 text-xl">招待グループ名：{{ $invtee_group_name }}</p>
                     <div class="mt-8">
-                        <form action="{{ route('group-user.update') }}" method="post">
+                        <form action="{{ route('group-user.accept') }}" method="post">
                             @csrf
                             @method('patch')
                             <button type="submit" name="participation_status" value="参加中" class="block w-full py-4 bg-slate-300 border-b-2 border-slate-200 hover:bg-sky-500 hover:text-slate-50">参加</button>
